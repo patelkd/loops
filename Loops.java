995c85bccd30dee0321547d17f16492609c28468
apcs <apcs@mvhs-u528-2.dcsdk12.org>
2013-09-03 09:05:56 -0600

    Made changed to loops and added homework

--- Loops.java
+++ Loops.java
@@ -1,18 +1,60 @@
 public class Loops {
 	public static void main(String[] args) {
-		
-		// int x = 1; //start
+
+
+
+		// String s = "My String";
+		// int length = findLength(s);
+		// System.out.println(length);
+
+
+		// int x = 1; //start	
 		// while (x <=100) {
 		// 	System.out.println(x);
 		// 	x++; 
 		// }
 
-   for (int i=1; i <= 100; i++) {
-   	System.out.println(i);
-   }
+		// for (int i=1; i <= 100; i++) {
+		// 	System.out.println(i);
+		// }
+
+		// int i=45;
+
+		// while (i>=20) {
+		// 	System.out.println(i);
+		// 	i=i-5;
+		// }
 
 
+		// for (int i=24; i<=68; i+=2) {
+		// 	System.out.println(i);
+			
+		// }
 
+	}
+
+
+	// public static int findLength(String str) {
+	// 	int l =0;
+	// 	for (int i=0; i<str.length(); i++) {
+	// 		len++;
+	// 	}
+
+	// 	return len;
+	// }
+	// 
+	
+	public static double DrugPotency(String str) {
+		double i=100.0;
+		double m=0;
+
+		while (i>=50.0) {
+			System.out.println("Month:" + m);
+			System.out.println("Effectiveness:" + i);
+
+			i=i-(i*.04);
+			m++;
+		}
 	}
 	
 }
--- README.md
+++ README.md
@@ -8,3 +8,13 @@ while and For loops
 #Latest Edit
 
 ##Last Edit
+
+##HW
+
+Attempt to get java and git working at home, bring questions on Thursday if it fails.
+
+Chapter 9 - Problems 3 and 5 - synched to GitHub
+
+Windows users download GitHub for Windows to use git. Also need to download and install java jdk.
+
+Command Line: set path variable for java. Or download and use Eclipse.
